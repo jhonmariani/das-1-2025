dc250061ef0caa7fd8c864f527c4f25a6e80b73c
Mariani <110943960+jhonmariani@users.noreply.github.com>
2025-05-03 14:25:35 -0300

    Delete eventos directory

--- eventos/src/main/java/br/univille/ativchat/Main.java
+++
@@ -1,9 +0,0 @@
-package br.univille.ativchat;
-
-import br.univille.ativchat.view.Form;
-
-public class Main {
-   public static void main(String[] args) {
-       new Form();
-   } 
-}
--- eventos/src/main/java/br/univille/ativchat/model/Mensagem.java
+++
@@ -1,8 +0,0 @@
-package br.univille.ativchat.model;
-
-public record Mensagem(String nome, String texto) {
-    public Mensagem(String nome, String texto) {
-        this.nome = nome;
-        this.texto = texto;
-    }
-} 
